e1a6967b63f5e22d4d6e423466f5ad8a12fadc32
albertot_dev <atejero.dam@gmail.com>
2022-06-08 14:44:06 +0200

    Testeando bot discord

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import MainLayout from 'layouts/main_layout'
 import '../styles/globals.css'
 import '../node_modules/highlight.js/styles/atom-one-dark.css'
+
 function MyApp({ Component, pageProps }) {
   if (Component.Layout) {
     const { Layout } = Component
