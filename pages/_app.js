856ed3ba8a368998025107bf41cb5a26404eaf0b
albertot_dev <atejero.dam@gmail.com>
2023-01-20 10:26:53 +0100

    added analytics and remove splitbee

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,30 @@
+import { Analytics } from '@vercel/analytics/react'
 import MainLayout from 'layouts/main_layout'
-import '../styles/globals.css'
-import '../node_modules/highlight.js/styles/atom-one-dark.css'
 import Head from 'next/head'
-import splitbee from '@splitbee/web'
-import { useEffect, useState } from 'react'
+import '../node_modules/highlight.js/styles/atom-one-dark.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  splitbee.init({
-    token: `86ZTHZVT02N9`
-  })
-
-  if (Component.Layout) {
-    const { Layout } = Component
-    return (
-      <>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
-          />
-        </Head>
-        <MainLayout>
+  const { Layout } = Component
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
+        />
+      </Head>
+      <MainLayout>
+        {Component.Layout ? (
           <Layout>
             <Component {...pageProps} />
           </Layout>
-        </MainLayout>
-      </>
-    )
-  }
-
-  return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </MainLayout>
+      <Analytics />
+    </>
   )
 }
 
