0df7fb146490fe070cf5c869663da2f1f00c4445
albertot_dev <atejero.dam@gmail.com>
2024-03-13 21:54:28 +0100

    Pivo 27 HomeLabbing

--- pages/proxima-pivo/index.js
+++ pages/proxima-pivo/index.js
@@ -10,7 +10,7 @@ export async function getServerSideProps({ res }) {
   return {
     redirect: {
       destination:
-        'https://saraos.tech/@xauendevs/events/xxvi-pivoandcode-homelabbing-tu-nube-en-casa-para-sobrevivir-a-la-edad-de-la-informacion-v7jydo4wo8dwhk29u8q13',
+        'https://saraos.tech/@xauendevs/events/xxvii-pivoandcode-homelabbing-tu-nube-en-casa-para-sobrevivir-a-la-edad-de-la-informacion-v7jydo4wo8dwhk29u8q13',
       permanent: true
     }
   }
