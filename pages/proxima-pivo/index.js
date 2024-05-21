c8de9a531fd1842ce24d880d25c6f8090e461672
rosmiagma <rmgm090882@hotmail.com>
2024-05-21 17:35:22 +0200

    Pivo 30 extreme programming

--- pages/proxima-pivo/index.js
+++ pages/proxima-pivo/index.js
@@ -10,7 +10,7 @@ export async function getServerSideProps({ res }) {
   return {
     redirect: {
       destination:
-        'https://saraos.tech/@xauendevs/events/xxix-pivoandcode-diseno-ui-ux-para-dummies-desarrolladores-dbp4clj8gddf96hzmh0mf',
+        'https://saraos.tech/@xauendevs/events/xxx-pivoandcode-extreme-programming-el-superpoder-que-necesitas-en-el-desarrollo-de-software-k5fwvt86k9774uzkcnfmc',
       permanent: true
     }
   }
