00a80b291dd3bad875a0f7fcea95639806117bc0
albertot-dev <atejero.dam@gmail.com>
2024-10-24 10:36:45 +0200

    proxima pivo

--- pages/proxima-pivo/index.js
+++ pages/proxima-pivo/index.js
@@ -10,7 +10,7 @@ export async function getServerSideProps({ res }) {
   return {
     redirect: {
       destination:
-        'https://saraos.tech/@xauendevs/events/31-pivoandcode-arquitecturas-limpias-separacion-del-negocio-y-los-detalles-de-la-infraestructura-umv2eugtimhvdq4f126d6',
+        'https://saraos.tech/@xauendevs/events/32-pivoandcode-la-informatizacion-de-la-agricultura-iwik9pm7t8nsj05e5l6n8',
       permanent: true
     }
   }
