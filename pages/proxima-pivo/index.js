9c692fdc5b7a2aaf1208ca4fc483e72db6897cae
albertot-dev <atejero.dam@gmail.com>
2024-11-13 09:10:31 +0100

    pivo 33

--- pages/proxima-pivo/index.js
+++ pages/proxima-pivo/index.js
@@ -10,7 +10,7 @@ export async function getServerSideProps({ res }) {
   return {
     redirect: {
       destination:
-        'https://saraos.tech/@xauendevs/events/32-pivoandcode-la-informatizacion-de-la-agricultura-iwik9pm7t8nsj05e5l6n8',
+        'https://saraos.tech/@xauendevs/events/33-pivoandcode-bugs-brechas-y-bounties-estamos-preparados-3uk0naefcw388z1tji1l6',
       permanent: true
     }
   }
